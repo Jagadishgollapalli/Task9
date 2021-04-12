0b7d7d768730b0fded5c6222fd7aeed4308c330d
Jagadishgollapalli <77227327+Jagadishgollapalli@users.noreply.github.com>
2021-04-12 11:39:34 +0530

    Add files via upload

---
+++ callbackhell.js
@@ -0,0 +1,35 @@
+let a=10;
+let i=0;
+setTimeout(()=>{
+    document.getElementById("display").innerHTML=a-i;i++;
+    setTimeout(()=>{
+    document.getElementById("display").innerHTML=a-i;i++;
+    setTimeout(()=>{
+        document.getElementById("display").innerHTML=a-i;i++;
+        setTimeout(()=>{
+            document.getElementById("display").innerHTML=a-i;i++;
+            setTimeout(()=>{
+                document.getElementById("display").innerHTML=a-i;i++;
+                setTimeout(()=>{
+                    document.getElementById("display").innerHTML=a-i;i++;
+                    setTimeout(()=>{
+                        document.getElementById("display").innerHTML=a-i;i++;
+                        setTimeout(()=>{
+                            document.getElementById("display").innerHTML=a-i;i++;
+                            setTimeout(()=>{
+                                document.getElementById("display").innerHTML=a-i;i++;
+                                setTimeout(()=>{
+                                    document.getElementById("display").innerHTML=a-i;i++;
+                                setTimeout(()=>{
+                                    document.getElementById("display").innerHTML="Happy Indipendence Day !";
+                                },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
+},1000);
